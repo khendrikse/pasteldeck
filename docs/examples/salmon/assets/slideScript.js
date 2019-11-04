4843212c23e412ef7861e5d33ad1bf71bdd6a9a6
khendrikse <30577427+khendrikse@users.noreply.github.com>
2019-11-04 14:21:48 +0100

    docs(examples): add more examples

---
+++ docs/examples/salmon/assets/slideScript.js
@@ -0,0 +1,26 @@
+const slides = document.getElementsByTagName('section');
+
+function navigate(currentLocation, boundary, location) {
+  if (currentLocation === boundary) return;
+  return (window.location.hash = location);
+}
+
+function navigateSection(e) {
+  const direction = checkKeyCode(e);
+  const currentLocation = parseInt(window.location.hash.slice(1) || 0);
+
+  if (direction === 'previous') {
+    navigate(currentLocation, 0, currentLocation - 1);
+  }
+
+  if (direction === 'next') {
+    navigate(currentLocation, slides.length - 1, currentLocation + 1);
+  }
+}
+
+function checkKeyCode(e) {
+  if (e.keyCode === 37 || e.keyCode === 38) return 'previous';
+  if (e.keyCode === 39 || e.keyCode === 40) return 'next';
+}
+
+document.addEventListener('keydown', navigateSection);
